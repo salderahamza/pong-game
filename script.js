84bd046b9794468267e057d9b66d2783f403c09e
dev-zarl <hamzasafdark@gmail.com>
2020-10-10 19:47:36 +0500

    canvas added

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Canvas
 const { body } = document;
-
-
+const canvas = document.createElement("canvas");
+const context = canvas.getContext("2d");
 const width = 500;
 const height = 700;
 const screenWidth = window.screen.width;
@@ -49,37 +49,37 @@ const winningScore = 7;
 
 // Render Everything on Canvas
 function renderCanvas() {
-  // Canvas Background
-  // context.fillStyle = 'black';
-  // context.fillRect(0, 0, width, height);
+  //Canvas Background
+  context.fillStyle = 'black';
+  context.fillRect(0, 0, width, height);
 
-  // Paddle Color
-  // context.fillStyle = 'white';
+  //Paddle Color
+  context.fillStyle = 'white';
 
-  // Player Paddle (Bottom)
-  // context.fillRect(paddleBottomX, height - 20, paddleWidth, paddleHeight);
+  //Player Paddle (Bottom)
+  context.fillRect(paddleBottomX, height - 20, paddleWidth, paddleHeight);
 
-  // Computer Paddle (Top)
-  // context.fillRect(paddleTopX, 10, paddleWidth, paddleHeight);
+  //Computer Paddle (Top)
+  context.fillRect(paddleTopX, 10, paddleWidth, paddleHeight);
 
-  // Dashed Center Line
-  // context.beginPath();
-  // context.setLineDash([4]);
-  // context.moveTo(0, 350);
-  // context.lineTo(500, 350);
-  // context.strokeStyle = 'grey';
-  // context.stroke();
+  //Dashed Center Line
+  context.beginPath();
+  context.setLineDash([4]);
+  context.moveTo(0, 350);
+  context.lineTo(500, 350);
+  context.strokeStyle = 'grey';
+  context.stroke();
 
-  // Ball
-  // context.beginPath();
-  // context.arc(ballX, ballY, ballRadius, 2 * Math.PI, false);
-  // context.fillStyle = 'white';
-  // context.fill();
+  //Ball
+  context.beginPath();
+  context.arc(ballX, ballY, ballRadius, 2 * Math.PI, false);
+  context.fillStyle = 'white';
+  context.fill();
 
-  // Score
-  // context.font = '32px Courier New';
-  // context.fillText(playerScore, 20, canvas.height / 2 + 50);
-  // context.fillText(computerScore, 20, canvas.height / 2 - 30);
+  //Score
+  context.font = '32px Courier New';
+  context.fillText(playerScore, 20, canvas.height / 2 + 50);
+  context.fillText(computerScore, 20, canvas.height / 2 - 30);
 }
 
 // Create Canvas Element
